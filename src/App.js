e08d0377ba9f6b27a201d3ecdcc5edc7a2b6bc5e
User <user@hackerrank.workspace>
2024-12-14 13:53:28 +0000

    Add initial repository
    Commit by HackerRank

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'h8k-components';
+import PollManager from './components/PollManager';
+
+const title = "Poll Manager";
+
+function App() {
+  return (
+    <div className="App">
+      <h8k-navbar header={title} data-testId="navbar"></h8k-navbar>
+      <PollManager />
+    </div>
+  );
+}
+
+export default App;
